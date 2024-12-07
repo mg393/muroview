b92d19bc8070fe14b7c8160d0a3c5c66dd3de262
Michael G <michael_gilmore@live.com>
2024-12-08 00:24:10 +0100

    muropeptide building getting there

--- muropeptide.py
+++ muropeptide.py
@@ -1,5 +1,12 @@
 from rdkit.Chem import rdchem, rdmolfiles, Descriptors, rdMolDescriptors
+from rdkit.Chem import rdchem, Draw, AllChem, Descriptors
+from rdkit.Chem.Draw import rdMolDraw2D
+
 from pyteomics import mass
+
+from PyQt5.QtSvg import QSvgWidget, QSvgRenderer
+from PyQt5.QtGui import QPixmap, QPalette, QColor, QImage, QPainter
+from PyQt5.QtCore import Qt, QByteArray
 
 class GlcNAc():
     def __init__(self):
@@ -13,12 +20,8 @@ class GlcNAc():
         self.OAc = oacetylated
 
     def smiles(self):
-        if self.NDeAc:
-            print("yeah boi")
-        elif self.OAc:
-            print("yeah boi")
-        elif self.NDeAc and self.OAc:
-            print("yeah boi")
+        if self.NDeAc == True:
+            return "N[C@@H]1[C@H]([C@@H]([C@H](OC1O(*))CO)O)O"
         else: 
             return "CC(=O)N[C@@H]1[C@H]([C@@H]([C@H](OC1O(*))CO)O)O"
 
@@ -50,15 +53,23 @@ class MurNAc():
             print("yeah boi")
         else: 
             return "O=C(O)[C@H](O[C@H]1[C@H](*)[C@H](OC(O)[C@@H]1NC(=O)C)CO)C"
-        
+
+class AminoAcid():
+    def __init__(self):
+        print("TODO")
+
 
 class Muropeptide(rdchem.Mol):
     def __init__(self):
         self.glcnac = GlcNAc()
         self.murnac = MurNAc()
+        self.aa1 = AminoAcid()
+        self.calc()
 
+
+    def calc(self):
+        print(self.glcnac.smiles())
         combinedSmiles = self.glcnac.smiles() + "." + self.murnac.smiles()
-        print(combinedSmiles)
         muro = rdmolfiles.MolFromSmiles(combinedSmiles.replace('(*)', '9'))
         print(rdmolfiles.MolToSmiles(muro))
         rdchem.Mol.__init__(self, muro)
@@ -74,6 +85,25 @@ class Muropeptide(rdchem.Mol):
         self.m1_nve = -mass.calculate_mass(formula = self.formula, ion_type="M", charge = -1)
         self.m2_nve = -mass.calculate_mass(formula = self.formula, ion_type="M", charge = -2)
         self.m3_nve = -mass.calculate_mass(formula = self.formula, ion_type="M", charge = -3)
+
+    def makeSVG(self):
+        AllChem.Compute2DCoords(self)
+        drawer = rdMolDraw2D.MolDraw2DSVG(600, 600)
+        drawer.ClearDrawing()
+        rdMolDraw2D.PrepareAndDrawMolecule(drawer, self)
+        drawer.FinishDrawing()
+        svg = drawer.GetDrawingText()
+        return QByteArray(svg.encode('utf-8'))
+
+    def draw(self, canvas):
+        svg_renderer = QSvgRenderer(self.makeSVG())
+        img = QImage(canvas.size(), QImage.Format_ARGB32)
+        img.fill(Qt.white)
+        painter = QPainter(img)
+        svg_renderer.render(painter)
+        painter.end()
+        return QPixmap.fromImage(img)
+
         
 
         
--- murotab.py
+++ murotab.py
@@ -2,24 +2,25 @@ from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QWidget, QLabel, QHBoxLayout, QVBoxLayout, QComboBox
 
 class MuroTab(QWidget):
-	def __init__(self, muro, central_widget):
+	def __init__(self, muro, central_widget, canvas):
 		super().__init__()
 		self.muropeptide = muro
-		self.molecularWeightLabel = QLabel("<b>Average mass:</b> ")
-		self.monoisotopicMwLabel = QLabel("<b>Monoisotopic mass:</b> ")
+		self.canvas = canvas
+		self.molecularWeightLabel = QLabel()
+		self.monoisotopicMwLabel = QLabel()
 		self.space1Label = QLabel("")
 		self.pve_charge_Label = QLabel("<u><b>Positive Charge States</b></u>")
-		self.m1_pve_Label = QLabel("[M+H]<sup>+</sup>: ")
-		self.mNa_pve_Label = QLabel("[M+Na]<sup>+<sup/>: ")
-		self.mK_pve_label = QLabel("[M+K]<sup>+<sup/>: ")
+		self.m1_pve_Label = QLabel()
+		self.mNa_pve_Label = QLabel()
+		self.mK_pve_label = QLabel()
 		self.space2Label = QLabel("")
-		self.m2_pve_Label = QLabel("[M+2H]<sup>2+</sup>: ")
-		self.m3_pve_Label = QLabel("[M+3H]<sup>3+</sup>: ")
+		self.m2_pve_Label = QLabel()
+		self.m3_pve_Label = QLabel()
 		self.space3Label = QLabel("")
 		self.nve_charge_Label = QLabel("<u><b>Negative Charge States</b></u>")
-		self.m1_nve_Label = QLabel("[M-H]<sup>-</sup>: ")
-		self.m2_nve_Label = QLabel("[M-2H]<sup>2-</sup>: ")
-		self.m3_nve_Label = QLabel("[M-3H]<sup>3-</sup>: ")
+		self.m1_nve_Label = QLabel()
+		self.m2_nve_Label = QLabel()
+		self.m3_nve_Label = QLabel()
 
 		self.main_layout = QVBoxLayout(central_widget)
 		self.main_layout.setAlignment(Qt.AlignTop)
@@ -60,9 +61,31 @@ class MuroTab(QWidget):
 		self.main_layout.addWidget(self.m2_nve_Label)
 		self.main_layout.addWidget(self.m3_nve_Label)
 		self.setLayout(self.main_layout)
+		self.update()
+
+	def update(self):
+		self.muropeptide.calc()
+		self.molecularWeightLabel.setText("<b>Average mass:</b> " + str(round(self.muropeptide.averageMass, 4)))
+		self.monoisotopicMwLabel.setText("<b>Monoisotopic mass:</b> " + str(round(self.muropeptide.monoisotopicMass, 4)))
+		self.m1_pve_Label.setText("[M+H]<sup>+</sup>: " + str(round(self.muropeptide.m1_pve, 4)))
+		self.m2_pve_Label.setText("[M+2H]<sup>2+</sup>: " + str(round(self.muropeptide.m2_pve, 4)))
+		self.m3_pve_Label.setText("[M+3H]<sup>3+</sup>: " + str(round(self.muropeptide.m3_pve, 4)))
+		self.mNa_pve_Label.setText("[M+Na]<sup>+</sup>: " + str(round(self.muropeptide.mNa_pve, 4)))
+		self.mK_pve_label.setText("[M+K]<sup>+</sup>: " + str(round(self.muropeptide.mK_pve, 4)))
+		self.m1_nve_Label.setText("[M-H]<sup>-</sup>: " + str(round(-1*self.muropeptide.m1_nve, 4)))
+		self.m2_nve_Label.setText("[M-2H]<sup>2-</sup>: " + str(round(-1*self.muropeptide.m2_nve, 4)))
+		self.m3_nve_Label.setText("[M-3H]<sup>3-</sup>: " + str(round(-1*self.muropeptide.m3_nve, 4)))
+		self.canvas.setPixmap(self.muropeptide.draw(self.canvas))
+
 
 	def on_GlcNAc_dropdown_changed(self, index):
 		selected_item = self.GlcNAc_dropdown.itemText(index)
+		if selected_item == "N-deacetylated":
+			self.muropeptide.glcnac.setNAc(True)
+		else: 
+			self.muropeptide.glcnac.setNAc(False)
+		print(self.muropeptide.glcnac.NDeAc)
+		self.update()
 		print(f"GlcNAc dropdown changed: Selected {selected_item}")
 
 	def on_MurNAc_dropdown_changed(self, index):
